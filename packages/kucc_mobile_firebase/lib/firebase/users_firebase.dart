b13754866cc93f316a605466f6dfbb2d64a58186
Aadarsha Dhakal <braveboy2058@gmail.com>
2022-04-04 18:07:02 +0545

    Finished implementing Firebase APi

--- packages/kucc_mobile_firebase/lib/firebase/users_firebase.dart
+++ packages/kucc_mobile_firebase/lib/firebase/users_firebase.dart
@@ -1,17 +1,39 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:kucc_mobile_data/kucc_mobile_data.dart';
 
 class UsersFirebase implements UserData {
+  final FirebaseFirestore _firebaseFirestore;
+  final FirebaseStorage _firebaseStorage;
+
+  const UsersFirebase(this._firebaseFirestore, this._firebaseStorage);
+
   @override
-  Future<UserModel> getUserData(String uid) {
-    // TODO: implement getUserData
-    throw UnimplementedError();
+  Future<UserModel> getUserData(String uid) async {
+    DocumentSnapshot<Map<String, dynamic>> result =
+        await _firebaseFirestore.collection('users').doc(uid).get();
+    if (result.data() != null) {
+      return UserModel.fromJson(result.data()!);
+    } else {
+      throw UserDataFetchError();
+    }
   }
 
   @override
-  Future<void> updateProfilePhoto(File file, String uid) {
-    // TODO: implement updateProfilePhoto
-    throw UnimplementedError();
+  Stream<double> updateProfilePhoto(File file, String uid) {
+    return _firebaseStorage
+        .ref('users/profilephotos/$uid/')
+        .putFile(file)
+        .asStream()
+        .asyncMap(
+          (event) => (event.bytesTransferred / event.totalBytes) * 100,
+        );
+  }
+
+  @override
+  Future<void> setUserData(String uid, UserModel model) async {
+    await _firebaseFirestore.collection('users').doc(uid).set(model.toJson());
   }
 }
---
+++ packages/kucc_mobile_firebase/test/firebase/events_firebase_test.dart
@@ -0,0 +1,13 @@
+/*
+TEST CASES
+
+1. Fetch All Events
+2. Create RSVP
+3. Check RSVP
+4. RSVP Number Count
+5. CHeck for null value in banner, end_date, speakers,
+6. Fetch all attendees
+
+*/
+
+// TODO: Write all test cases
